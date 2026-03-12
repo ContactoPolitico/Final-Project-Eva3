c66178b0ec407a51abc4e1bcac0bcce86a7d1c1e
Federico <alvarezacostafederico@gmail.com>
2026-03-12 16:04:56 -0400

    Add country details and improve trip planning logic

--- TravelAgency.py
+++ TravelAgency.py
@@ -15,11 +15,15 @@ subregion = response.json()[0]["subregion"]
 population = response.json()[0]["population"]
 currency = response.json()[0]["currencies"][0]["name"]
 language = response.json()[0]["languages"][0]["name"]
+countrycode = response.json()[0]["callingCodes"]
+alpha3code = response.json()[0]["alpha3Code"]
+area = response.json()[0]["area"]
+timezones = response.json()[0]["timezones"]
 
 
 linkweather = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=weather_code&hourly=temperature_2m&current=temperature_2m&timezone=auto&start_date=2026-03-12&end_date=2026-03-13"
 
-country = [name, latitude, longitude, capital, region, subregion, population, currency, language]
+country = [name, latitude, longitude, capital, region, subregion, population, currency, language, countrycode, alpha3code, area, timezones]
 
 for info in country:
   print(info)
@@ -37,6 +41,10 @@ responsecountry = requests.get(linkcountry)
 trip = input("Let's create a travel plan for your journey, please state your name: ")
 tripcountries = input("Please state the countries you want to visit: ")
 tripdates = int(input("Please state the total amount of days you will stay in this/those country/countries you want to visit: "))
+if  tripdates > 1:
+	print("Ok, let's continue")
+else:
+	print("That is not a valid answer")
 notes = input("Are there any notes or special requirements you want to let us know?(answer yes or no): ")
 
 if notes == "yes":
@@ -47,15 +55,20 @@ else:
 	print("That is not a valid answer")
 
 countrieslist = tripcountries.split(",")
-
+for tripcountries in countrieslist:
+	tripcountries = tripcountries.strip()
+	
 priceAM = 30 #30$ a night
 priceEU = 40 #40$ a night
 priceAS = 25 #25$ a night
 priceAF = 30 #30$ a night
 priceOC = 35 #35$ a night
 
-for country in countrieslist:
-	country = country.strip()
+linkcountries = f"https://www.apicountries.com/name/{tripcountries}"
+
+ans = requests.get(linkcountries)
+
+region = ans.json()[0]["region"]
 
 if tripcountries == country["region"]["Americas"]:
 	print(f"That will be {tripdates * priceAM}$")
@@ -70,7 +83,7 @@ elif tripcountries == country["region"]["Oceania"]:
 else:
 	print("That is not a valid country")
 
-print(trip, tripcountries, tripdates)
+print(trip, tripcountries, tripdates, notes)
 
 response = (responsecountry)
 
