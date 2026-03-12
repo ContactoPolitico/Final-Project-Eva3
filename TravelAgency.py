bf0fb9a70818e61afa6aead74579fb73a7a4fe25
Saul Ruiz <srluna0205@yahoo.com>
2026-03-11 20:46:22 -0400

    le agregue al api del weather para ayudar el primer api
    agregue el api del weather y tambien le agregue algo para ayudar el primer api

--- TravelAgency.py
+++ TravelAgency.py
@@ -15,3 +15,19 @@ subregion = response.json()[0]["subregion"]
 population = response.json()[0]["population"]
 currency = response.json()[0]["currencies"][0]["name"]
 language = response.json()[0]["languages"][0]["name"]
+
+
+linkweather = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=weather_code&hourly=temperature_2m&current=temperature_2m&timezone=auto&start_date=2026-03-11&end_date=2026-03-13"
+
+country = [name, latitude, longitude, capital, region, subregion, population, currency, language]
+
+for info in country:
+  print(info)
+
+
+weatherresponse = requests.get(linkweather)
+weatherinfo = weatherresponse.json()
+weathercode = weatherinfo["hourly"]["temperature_2m"]
+
+for temperature in weathercode:
+  print(temperature)
